28cebeb68368284a495ff1501ec2e501f331e63f
Jose Carlos <josecarloscjj@gmail.com>
2022-12-24 02:02:29 -0300

    bug: Resource not found

--- src/main/java/br/com/banco/controllers/handlers/ControllerExceptionHandler.java
+++ src/main/java/br/com/banco/controllers/handlers/ControllerExceptionHandler.java
@@ -1,5 +1,6 @@
 package br.com.banco.controllers.handlers;
 
+import br.com.banco.services.exceptions.ResourceNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -12,9 +13,16 @@ import java.time.format.DateTimeParseException;
 @ControllerAdvice
 public class ControllerExceptionHandler {
   @ExceptionHandler(DateTimeParseException.class)
-  public ResponseEntity<CustomError> resourceNotFound(DateTimeParseException e, HttpServletRequest request) {
+  public ResponseEntity<CustomError> dateTimeParse(DateTimeParseException e, HttpServletRequest request) {
     HttpStatus status = HttpStatus.NOT_FOUND;
     CustomError error = new CustomError(Instant.now(), status.value(), "Datas inválidas", request.getRequestURI());
     return ResponseEntity.status(status).body(error);
   }
+  @ExceptionHandler(ResourceNotFoundException.class)
+  public ResponseEntity<CustomError> resourceNotFound(ResourceNotFoundException e, HttpServletRequest request) {
+    HttpStatus status = HttpStatus.NOT_FOUND;
+    CustomError error = new CustomError(Instant.now(), status.value(), e.getMessage(), request.getRequestURI());
+    return ResponseEntity.status(status).body(error);
+  }
+
 }
